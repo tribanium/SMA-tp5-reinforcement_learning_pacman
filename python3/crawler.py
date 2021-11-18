ed31b3b392c5e9330bc3557a579221d87187443d
Paul Flagel <paulflagel@gmail.com>
2021-11-18 17:28:30 +0100

    Changed float to int values for list index

--- python3/crawler.py
+++ python3/crawler.py
@@ -125,8 +125,8 @@ class CrawlingRobotEnvironment(environment.Environment):
         ## Also call self.crawlingRobot.setAngles()
         ## to the initial arm and hand angle
 
-        armState = self.nArmStates / 2
-        handState = self.nHandStates / 2
+        armState = self.nArmStates // 2
+        handState = self.nHandStates // 2
         self.state = armState, handState
         self.crawlingRobot.setAngles(
             self.armBuckets[armState], self.handBuckets[handState]
